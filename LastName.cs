4ca82fae8c0266d83582ec327f50f65698fc221c
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-10-24 16:58:30 +0530

    added lastname

---
+++ LastName.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegexDemo
+{
+    public class LastName
+    {
+        public string REGEX_NAME = "^[A-Z]{3,}$";
+
+        public bool ValidateLastName(string name)
+        {
+            return Regex.IsMatch(name, REGEX_NAME);
+        }
+    }
+}
+
+
+
+
+
