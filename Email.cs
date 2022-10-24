01aa18d6f57b70a82621acdf90ca5751f64b3190
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-10-24 17:16:52 +0530

    added email

---
+++ Email.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegexDemo
+{
+    public class Email
+    {
+        public string REGEX_Email = "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2,})?$";
+
+        public bool ValidateEmail(string name)
+        {
+            return Regex.IsMatch(name, REGEX_Email);
+        }
+    }
+}
+
+
+
+
+
