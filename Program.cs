18e036ba9ed1641fe6992244f195ff84de9e2b09
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-10-24 17:18:06 +0530

    added programcs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,19 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("welcome to user  restation problem");
+            Console.WriteLine("please enter the name");
+            string name = Console.ReadLine();
+            Email email = new Email();
+            bool val = email.ValidateEmail(name);
+            if (val)
+            {
+                Console.WriteLine("Mathed");
+            }
+            else
+            {
+                Console.WriteLine(" not Mathed");
+
+            }
         }
     }
 }
